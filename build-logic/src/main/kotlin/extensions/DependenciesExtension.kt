cca432567573e4c7bf18ba8e469d3abc1e1ba9a2
Babananick <babananick@gmail.com>
2024-12-02 20:22:25 +0300

    Removed web and desktop dependencies

--- build-logic/src/main/kotlin/extensions/DependenciesExtension.kt
+++ build-logic/src/main/kotlin/extensions/DependenciesExtension.kt
@@ -1,36 +1,39 @@
 package extensions
 
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.getByName
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 import org.jetbrains.kotlin.gradle.plugin.KotlinDependencyHandler
 
+internal val Project.kotlinMultiplatform: KotlinMultiplatformExtension
+    get() = extensions.getByName("kotlin") as KotlinMultiplatformExtension
+
 fun Project.commonMainDependencies(block: KotlinDependencyHandler.() -> Unit) =
-    with(extensions.getByName<KotlinMultiplatformExtension>("kotlin")) {
+    with(kotlinMultiplatform) {
         sourceSets.commonMain.dependencies(block)
     }
 
 fun Project.androidMainDependencies(block: KotlinDependencyHandler.() -> Unit) =
-    with(extensions.getByName<KotlinMultiplatformExtension>("kotlin")) {
+    with(kotlinMultiplatform) {
         sourceSets.androidMain.dependencies(block)
     }
 
 fun Project.iosMainDependencies(block: KotlinDependencyHandler.() -> Unit) =
-    with(extensions.getByName<KotlinMultiplatformExtension>("kotlin")) {
+    with(kotlinMultiplatform) {
         sourceSets.iosMain.dependencies(block)
     }
 
+
 fun Project.commonTestDependencies(block: KotlinDependencyHandler.() -> Unit) =
-    with(extensions.getByName<KotlinMultiplatformExtension>("kotlin")) {
+    with(kotlinMultiplatform) {
         sourceSets.commonTest.dependencies(block)
     }
 
 fun Project.androidTestDependencies(block: KotlinDependencyHandler.() -> Unit) =
-    with(extensions.getByName<KotlinMultiplatformExtension>("kotlin")) {
+    with(kotlinMultiplatform) {
         sourceSets.androidUnitTest.dependencies(block)
     }
 
 fun Project.iosTestDependencies(block: KotlinDependencyHandler.() -> Unit) =
-    with(extensions.getByName<KotlinMultiplatformExtension>("kotlin")) {
+    with(kotlinMultiplatform) {
         sourceSets.iosTest.dependencies(block)
     }
