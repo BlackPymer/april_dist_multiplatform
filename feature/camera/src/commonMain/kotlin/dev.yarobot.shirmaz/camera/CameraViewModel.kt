eaa6bd4e6c0fce766df90d70fad3e9f4776312bb
Maksim Palchevski <83406787+YraBot@users.noreply.github.com>
2025-01-11 20:47:31 +0300

    fixed CameraViewModel.kt bug with permission

--- feature/camera/src/commonMain/kotlin/dev.yarobot.shirmaz/camera/CameraViewModel.kt
+++ feature/camera/src/commonMain/kotlin/dev.yarobot.shirmaz/camera/CameraViewModel.kt
@@ -10,39 +10,39 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
-class CameraViewModel: MVIViewModel<CameraIntent, CameraScreenState>() {
+class CameraViewModel : MVIViewModel<CameraIntent, CameraScreenState>() {
     private val _state = MutableStateFlow(
         CameraScreenState(
             cameraProvideState = CameraProvideState.NotGranted
         )
     )
     override val state = _state.asStateFlow()
-
     override fun onIntent(intent: CameraIntent) {
         when (intent) {
             is CameraIntent.RequestCamera -> intent.permissionsController.requestCamera()
-            is CameraIntent.CheckCameraPermission -> intent.permissionsController
-                .proceedCameraState()
-        }
-    }
-
-    private fun PermissionsController.requestCamera(){
-        viewModelScope.launch {
-            this@requestCamera.providePermission(Permission.CAMERA)
-        }
-        this.proceedCameraState()
-    }
-
-    private fun PermissionsController.proceedCameraState() =
-        viewModelScope.launch {
-            when (this@proceedCameraState.getPermissionState(Permission.CAMERA)) {
-                PermissionState.Granted -> {
-                    _state.update {
-                        it.copy(cameraProvideState = CameraProvideState.Granted)
-                    }
-                }
-
-                else -> _state.update { it.copy(cameraProvideState = CameraProvideState.NotGranted) }
+            is CameraIntent.CheckCameraPermission -> viewModelScope.launch {
+                intent.permissionsController
+                    .proceedCameraState()
             }
         }
+    }
+
+    private fun PermissionsController.requestCamera() {
+        viewModelScope.launch {
+            this@requestCamera.providePermission(Permission.CAMERA)
+            proceedCameraState()
+        }
+    }
+
+    private suspend fun PermissionsController.proceedCameraState() =
+        when (this@proceedCameraState.getPermissionState(Permission.CAMERA)) {
+            PermissionState.Granted -> {
+                _state.update {
+                    it.copy(cameraProvideState = CameraProvideState.Granted)
+                }
+            }
+
+            else -> _state.update { it.copy(cameraProvideState = CameraProvideState.NotGranted) }
+        }
+
 }
