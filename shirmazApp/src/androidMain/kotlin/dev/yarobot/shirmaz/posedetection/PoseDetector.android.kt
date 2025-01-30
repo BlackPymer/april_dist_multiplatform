761209a0542d805a898bd85f3a13990e7fe768c7
Maksim Palchevski <83406787+YraBot@users.noreply.github.com>
2025-01-30 18:43:42 +0300

    Added processImage function for android

--- shirmazApp/src/androidMain/kotlin/dev/yarobot/shirmaz/posedetection/PoseDetector.android.kt
+++ shirmazApp/src/androidMain/kotlin/dev/yarobot/shirmaz/posedetection/PoseDetector.android.kt
@@ -1,5 +1,10 @@
 package dev.yarobot.shirmaz.posedetection
 
+import androidx.annotation.OptIn
+import androidx.camera.core.ExperimentalGetImage
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.pose.PoseDetection
+import com.google.mlkit.vision.pose.defaults.PoseDetectorOptions
 import dev.yarobot.shirmaz.platform.PlatformError
 import dev.yarobot.shirmaz.platform.PlatformImage
 import dev.yarobot.shirmaz.platform.PlatformLandmark
@@ -9,11 +14,29 @@ actual fun createPoseDetector(options: ShirmazPoseDetectorOptions): ShirmazPoseD
         override val options: ShirmazPoseDetectorOptions
             get() = options
 
+
+        @OptIn(ExperimentalGetImage::class)
         override fun processImage(
             image: PlatformImage,
             onProcess: (List<PlatformLandmark>?, PlatformError?) -> Unit
         ) {
-
+            val poseDetectorOptions = PoseDetectorOptions.Builder()
+                .setDetectorMode(PoseDetectorOptions.STREAM_MODE)
+                .build()
+            val poseDetector = PoseDetection.getClient(poseDetectorOptions)
+            val inputImage =
+                InputImage.fromMediaImage(image.image!!, image.imageInfo.rotationDegrees)
+            poseDetector.process(inputImage)
+                .addOnSuccessListener { pose ->
+                    val results = pose.allPoseLandmarks
+                    onProcess(results, null)
+                }
+                .addOnFailureListener { error ->
+                    onProcess(null, error)
+                }
+                .addOnCompleteListener {
+                    image.close()
+                }
         }
     }
 }
